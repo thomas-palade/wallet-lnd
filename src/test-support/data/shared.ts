541903b62cd797378e4ec53509bd893ec0455bc2
thomas_palade <thomas.palade@vgw.co>
2022-06-28 18:36:17 +0300

    feat(wallet): add trests

--- src/test-support/data/shared.ts
+++ src/test-support/data/shared.ts
@@ -5,5 +5,6 @@ export const CREATED_AT = randomDate();
 export const ID = randomInteger();
 export const COINS = 5000;
 export const TRANSACTION_ID = 'tx102';
+export const NEW_TRANSACTION_ID = 'tx103';
 export const UPDATED_AT = randomDate();
 export const CURRENCY = CURRENCIES.SWEEPS;
--- src/wallets/api/credit-wallet-by-id.spec.ts
+++ src/wallets/api/credit-wallet-by-id.spec.ts
@@ -5,6 +5,8 @@ import { getClient, getNewPool, close } from '../../lib/postgres/pool';
 import { deleteAllTables } from '../../test-support/database';
 import request from 'supertest';
 import { WALLET_ID } from '../../test-support/data/wallet';
+import { COINS, NEW_TRANSACTION_ID, TRANSACTION_ID } from '../../test-support/data';
+import { createWallet, findAllWallets } from '../wallet';
 
 let client: PoolClient;
 let server: StoppableServer;
@@ -23,9 +25,76 @@ test.after(() => {
   close(pool);
 });
 
-test('When `POST` `/v1/wallets/:walletId/credit` is hit, Then it returns a 200 status', async (t) => {
-  const response = await request(server)  
+test('When `POST` `/v1/wallets/:walletId/credit` is hit with an unknown `walletId`, Then it returns a wallet that has the balance equal to the credited coins', async (t) => {
+  const response = await request(server)
     .post(`/v1/wallets/${WALLET_ID}/credit`)
-    .send({});
-  t.is(response.status, 200);
+    .send({
+      transactionId: TRANSACTION_ID,
+      coins: COINS
+    });
+  t.is(response.status, 201);
+  t.is(response.body, {
+    transactionId: TRANSACTION_ID,
+    coins: COINS
+  });
+});
+
+test('When `POST` `/v1/wallets/:walletId/credit` is hit with an unknown `walletId`, Then it creates a wallet in the database that contains the coins equal to the credited coins', async (t) => {
+  await request(server)
+    .post(`/v1/wallets/${WALLET_ID}/credit`)
+    .send({
+      transactionId: TRANSACTION_ID,
+      coins: COINS
+    });
+  const newWallet = (await findAllWallets(client))[0];
+  t.deepEqual({
+    transactionId: newWallet.transactionId,
+    coins: newWallet.coins
+  }, {
+    transactionId: TRANSACTION_ID,
+    coins: COINS
+  });
+});
+
+test('Given an existing wallet associated with a given `walletId`, When `GET` `/v1/wallets/:walletId/`, Then it returns a 201 status and a `WalletObject` that contains the updated coins balance', async (t) => {
+  const { id: walletId } = await createWallet(
+    client,
+    COINS,
+    TRANSACTION_ID
+  );
+  const EXTRA_CREDITED_COINS = 20;
+  const response = await request(server)
+    .post(`/v1/wallets/${walletId}/credit`)
+    .send({
+      transactionId: NEW_TRANSACTION_ID,
+      coins: EXTRA_CREDITED_COINS
+    });
+  t.is(response.status, 201);
+  t.deepEqual(response.body, {
+    transactionId: NEW_TRANSACTION_ID,
+    coins: COINS + EXTRA_CREDITED_COINS
+  });
+});
+
+test('Given an existing wallet associated with a given `walletId`, When `GET` `/v1/wallets/:walletId/`, Then it returns a 201 status and a `WalletObject` that contains the updated coins balance', async (t) => {
+  const { id: walletId } = await createWallet(
+    client,
+    COINS,
+    TRANSACTION_ID
+  );
+  const EXTRA_CREDITED_COINS = 20;
+  await request(server)
+    .post(`/v1/wallets/${walletId}/credit`)
+    .send({
+      transactionId: NEW_TRANSACTION_ID,
+      coins: EXTRA_CREDITED_COINS
+    });
+  const updatedWallet = (await findAllWallets(client))[0];
+  t.deepEqual({
+    transactionId: updatedWallet.transactionId,
+    coins: updatedWallet.coins
+  }, {
+    transactionId: NEW_TRANSACTION_ID,
+    coins: COINS + EXTRA_CREDITED_COINS
+  });
 });
